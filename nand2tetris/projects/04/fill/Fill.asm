cc2866dadf8e768b94713279c99d4cd125ca69a0
Luminoid <14302010006@fudan.edu.cn>
2016-10-07 23:57:29 +0800

    nand2tetris Project4

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -10,3 +10,53 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
+
+(INIT)
+// R0(pixel)=screen
+@SCREEN
+D=A
+@R0
+M=D
+// R1(pressed)=0
+@R1
+M=0
+
+// Get key pressed event
+(KEYPRESSED)
+@KBD
+D=M
+@UNPRESSED
+D;JEQ
+
+// Set pressed value
+(PRESSED)
+@R1
+M=-1
+@LOOP
+0;JMP
+(UNPRESSED)
+@R1
+M=0
+
+(LOOP)
+// if(R0==16384+8192) goto END
+@24576
+D=A
+@R0
+D=D-M
+@INIT
+D;JEQ
+
+// RAM[SCREEN+i]=pressed
+@R1
+D=M
+@R0
+A=M
+M=D
+
+// i++
+@R0
+M=M+1
+
+@LOOP
+0;JMP
--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -6,18 +6,21 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
+@R2
+M=0     // R2=0
+
 (LOOP)
 @R1
-D=M-1   // D=R1-1
+D=M     // D=R1
 @END
-D;JLT   // If (R1-1)<0 goto END
+D;JEQ   // If R1=0 goto END
 @R1
 M=M-1   // R1=R1-1
 @R0
 D=M     // D=R0
 @R2
 M=D+M   // R2=R0+R2
-@loop
+@LOOP
 0;JMP   // goto LOOP
 
 (END)
